3bf06ce387f74ba4195f040385421d0a7e1338f2
biny235 <biny235@users.noreply.github.com>
2018-08-07 12:35:49 -0400

    fixing bugs

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(require('body-parser').json());
+app.use(require('body-parser').json({limit: '50mb'}));
 
 app.use('/', express.static(path.join(__dirname, '../client')));
 app.get('/', (req, res, next) => {
