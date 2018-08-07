1fa56cf232b6aaabc6a1a06dab6f1bfa57f9767c
biny235 <biny235@users.noreply.github.com>
2018-08-07 12:11:41 -0400

    commit

--- server/api.js
+++ server/api.js
@@ -33,7 +33,25 @@ router.post('/download/:type', (req, res, next)=>{
   let { data } = req.body;
   let type = req.params.type;
   fileHandler[type](data);
-  res.sendFile(path.join(__dirname, `../analysys.${type}`))
+  res.download(path.join(__dirname, `../analysys.${type}`))
+});
+
+// Routes for downloaded from file upload (not yet implemented on front end)
+router.post('/analyze/file/download/:type', upload.single('file'), (req, res, next)=>{
+  const {file} = req;
+  if(file){
+    fs.readFile(file.path, 'utf8', (err, data)=>{
+      if(err) next(err)
+      data = analyser(JSON.parse(data));
+      fileHandler[req.params.type](data)
+      res.download(path.join(__dirname, `../analysys.${type}`));
+    });
+  }
+});
+router.post('/analyze/JSON/download/:type', (req, res, next)=>{
+  let data = analyser(req.body)
+  fileHandler[req.params.type](data)
+  res.download(path.join(__dirname, `../analysys.${type}`));
 })
 
 
